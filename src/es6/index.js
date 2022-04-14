922bac8839a6def1d5fa253910edc8a4b0b6d71c
Devx93 <goyox93@gmail.com>
2022-04-14 15:11:55 -0500

    se agrego la misma funcion con referencia a es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,7 +1,15 @@
 function  newFunction(name, age, country) {
     var name = name || 'goyo';
-    var age = age || 32;
+    var age = age || 29;
     var country = country || 'EZL';
     console.log(name, age, country);
 }
 
+//es6
+
+function newFunction2(name = 'goyo', age = 29, country = 'EZL') {
+    console.log(name, age, country);
+}
+
+newFunction2(); // llamado sin elementos
+newFunction2('Ricardo', '23', 'COL');
