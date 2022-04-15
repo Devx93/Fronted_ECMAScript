3ceb8643499b62491fee002497e10c299c42a2b2
Devx93 <goyox93@gmail.com>
2022-04-15 10:04:01 -0500

    tranajando con regex

--- src/es9/index.js
+++ src/es9/index.js
@@ -21,3 +21,27 @@ const obj2 = {
 };
 
 console.log(obj2);
+
+//
+
+const helloWorld = () => {
+return new Promise((resolve, reject) => {
+    (true)
+     ? resolve('Hello World')
+     : reject(new Error('Test Error'))
+});
+};
+
+helloWorld()
+ .then(response => console.log(response))
+ .catch(error => console.log(error))
+ .finally(() => console.log('FInalizo'))
+
+
+ const regexData = /([0-9]{4})-([0-9]{2})-([0-90]{2})/
+ const match = regexData.exec('2022-04-15');
+ const year = match[1]
+ const month = match[2]
+ const day = match[3]
+
+ console.log(year, month, day);
