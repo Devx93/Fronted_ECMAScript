78808a1cb4a9f0973a8befbf9e95d4b348387fb7
Devx93 <goyox93@gmail.com>
2022-04-15 08:36:05 -0500

    trabajando con async y await

--- src/es8/index.js
+++ src/es8/index.js
@@ -28,3 +28,20 @@ console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, '-----'))
 
 console.log('food'.padEnd(12, '----'))
+
+//async await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+         ? setTimeout(() => resolve('Hello World'), 3000)
+         : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
