86fbc91a57442ee3cbcb154c38021e5ca784ad6a
Devx93 <goyox93@gmail.com>
2022-04-14 19:53:02 -0500

    clases y constructores

--- src/es6/index.js
+++ src/es6/index.js
@@ -130,4 +130,21 @@ helloPromise()
     //.then(() => console.log('hola'))
     .catch(error => console.log(error));
 
+// clases y constructores
 
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+//
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
