bb48bd439f0f77a32e89f32a10cd1865aaf3c16a
Devx93 <goyox93@gmail.com>
2022-04-14 20:07:44 -0500

    ejemplo de elevar a la potencia

--- src/es7/index.js
+++ src/es7/index.js
@@ -5,3 +5,11 @@ if (numbers.includes(7)) {
 } else {
     console.log('No se encuentra.');
 }
+
+// elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
