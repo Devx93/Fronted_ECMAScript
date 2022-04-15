c02fb743db94b4bd26984418168ae1f26f7f44ea
Devx93 <goyox93@gmail.com>
2022-04-15 12:37:44 -0500

    trabajando numero grande

--- src/es11/index.js
+++ src/es11/index.js
@@ -4,3 +4,10 @@ button.addEventListener("click", async function () {
     const module = await import("./file.js");
     module.hello();
 });
+
+//
+
+const aBigNumber = 9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
